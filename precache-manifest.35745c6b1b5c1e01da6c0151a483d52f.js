d07d4cc3e24f8554e86c610da46dc4d6a00ba2cb
Atul Bisht <atulbisht26@gmail.com>
2019-01-26 11:26:21 +0530

    Updates

---
+++ precache-manifest.35745c6b1b5c1e01da6c0151a483d52f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad0e81317313131db11a40da53631843",
+    "url": "/club-website-frontend/static/media/discuss.ad0e8131.png"
+  },
+  {
+    "revision": "ef99e519e7e3fed571ab",
+    "url": "/club-website-frontend/static/js/runtime~main.ef99e519.js"
+  },
+  {
+    "revision": "aba9245414df77c93f85",
+    "url": "/club-website-frontend/static/js/main.aba92454.chunk.js"
+  },
+  {
+    "revision": "39212575aec19df34e12",
+    "url": "/club-website-frontend/static/js/1.39212575.chunk.js"
+  },
+  {
+    "revision": "aba9245414df77c93f85",
+    "url": "/club-website-frontend/static/css/main.508c91cd.chunk.css"
+  },
+  {
+    "revision": "70a4868dd689f17088f4c2c9fc25462c",
+    "url": "/club-website-frontend/index.html"
+  }
+];
