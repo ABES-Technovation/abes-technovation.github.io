11f270b1a68c8d54ee93c4ccb4878f6cf22efaf2
Atul Bisht <atulbisht26@gmail.com>
2019-01-28 03:00:26 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.616418e92b4e1ee17dad76c7a4545cab.js"
+  "/ABES-Technovation/abes-technovation.github.io/precache-manifest.19dbe69139f73395bb3d3ab78b999aab.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/ABES-Technovation/abes-technovation.github.io/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
